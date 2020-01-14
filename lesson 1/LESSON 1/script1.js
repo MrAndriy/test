b2bfb731697429bff56672fb4ca14bff64c32714
MrAndriy <59733657+MrAndriy@users.noreply.github.com>
2020-01-14 14:20:47 +0200

    Add files via upload

---
+++ lesson 1/LESSON 1/script1.js
@@ -0,0 +1,75 @@
+// task № 2
+// console.log("MRIY");
+
+// task № 3
+// var x = 10;              //3 a), b) declared two variables
+// var y = 150;            //3 a), b) wrote down any values ​​in the variable
+// console.log(x);        //3 c) displays the value х
+// console.log(y);       // 3 c) displays the value у
+
+// x = y;                // 3. d) copied the value of one variable to another
+// console.log(x);      // 3. e)display the values ​​of the variables
+// console.log(y);     // 3. e)display the values ​​of the variables
+
+//task № 4
+// var isAdult = confirm("are you Adult?");
+// console.log(isAdult);                      //displays the user's response
+
+// task № 5
+
+// var firstName = "Andriy";
+// var lastName = "Mriy";
+// var groupNumber = "Lv-476.JS Core";
+// var birthYear = 1992;
+// var maritalStatus = confirm("are you meried?");
+// var email = null;
+// var experience;
+
+// console.log("birthYear " + birthYear); //consolidates birthYear
+// console.log(typeof birthYear); // console by type is "number"
+
+// console.log("maritalStatus " + "is " + maritalStatus); //consolidates maritalStatus
+// console.log(typeof maritalStatus); // console by type is "Boolean"
+
+// console.log("groupNumber " + groupNumber); //consolidates groupNumber
+// console.log(typeof groupNumber); // console by type is "string"
+
+// console.log("firstName " + firstName); //consolidates firstName
+// console.log(typeof firstName); // console by type is "string"
+
+// console.log("lastName " + lastName); //consolidates lastName
+// console.log(typeof lastName); // console by type is "string"
+
+// console.log("email " + "is " + email); //consolidates email
+// console.log(typeof email); // console by type is null but js read as "object"  JS eror =)
+
+// console.log("experience " + "is " + experience); //consolidates experience
+// console.log(typeof experience); // console by type is "undefined"
+
+//task № 6
+// var login = prompt("Please enter your login", ">enter here<");
+// var email = prompt("Please enter your email", ">enter here<");
+// var password = prompt("Please enter your password", ">enter here<");
+// alert(
+//   "Dear " +
+//     login +
+//     ", your email is " +
+//     email +
+//     " , your password is " +
+//     password
+// );
+
+//task № 7 timeViewer
+// var seconds = 1;
+// var oneMinutes = 60;
+// oneMinutes *= seconds;
+// var oneHour = 60;
+// oneHour *= oneMinutes;
+// var hoursPerDay = 24;
+// hoursPerDay *= oneHour;
+// var daysInMoth = 31;
+// daysInMoth *= hoursPerDay;
+
+// console.log("secondsPerHour " + oneHour);
+// console.log("secondsPerDay " + hoursPerDay);
+// console.log("secondsPerMonth " + daysInMoth);
